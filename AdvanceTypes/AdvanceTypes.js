98a5a4e44a8402f7b4618d410cc4cd17db431c59
bhanu aluka <alukabhanuprakash@gmail.com>
2025-04-05 10:43:17 +0530

    interfaces

--- AdvanceTypes/AdvanceTypes.js
+++ AdvanceTypes/AdvanceTypes.js
@@ -1,4 +1,51 @@
+// Function type in ts
 function basic(a, b) {
     return a + b;
 }
 console.log(basic(10, 10));
+// void: Void Indicates that function returns nothing
+//   The function doesn’t return anything (but it finishes its job).
+function Void() {
+    console.log("nothing");
+}
+Void();
+// Optional parameters in function
+function OptionalParameters(a, b, c) {
+    return a + b;
+}
+OptionalParameters(10, 20);
+// never:
+// function error()
+// never ends
+/* The function does not finish its work and come back to the place it was called from.
+
+
+Keyword	What it Means	Example
+void	Function does something but returns nothing	A greeting or console log function
+never	Function never ends normally (error or loop)	Error throwing or infinite loop */
+function NeverEnds() {
+    throw new Error("Something went wrong");
+}
+NeverEnds();
+function NeverEnding() {
+    while (true) {
+        console.log("It never Ends ");
+    }
+}
+NeverEnding();
+const personObj = {
+    name: "bhanu",
+    age: 24,
+    isAttended: true,
+};
+console.log(personObj.age);
+const twoSum = (a, b) => {
+    return a + b;
+};
+console.log(twoSum(10, 20));
+const User = {
+    name: "bhanu",
+    age: 24,
+    isAttended: true,
+};
+let Multmessage = 6546545;
--- AdvanceTypes/AdvanceTypes.ts
+++ AdvanceTypes/AdvanceTypes.ts
@@ -6,6 +6,7 @@ function basic(a: number, b: number): number {
 console.log(basic(10, 10));
 
 // void: Void Indicates that function returns nothing
+//   The function doesn’t return anything (but it finishes its job).
 function Void(): void {
   console.log("nothing");
 }
@@ -13,9 +14,81 @@ function Void(): void {
 Void();
 
 // Optional parameters in function
-
 function OptionalParameters(a: number, b: number, c?: number): number {
   return a + b;
 }
 
 OptionalParameters(10, 20);
+
+// never:
+// function error()
+
+// never ends
+/* The function does not finish its work and come back to the place it was called from.
+
+
+Keyword	What it Means	Example
+void	Function does something but returns nothing	A greeting or console log function
+never	Function never ends normally (error or loop)	Error throwing or infinite loop */
+
+function NeverEnds(): never {
+  throw new Error("Something went wrong");
+}
+
+NeverEnds();
+
+function NeverEnding() {
+  while (true) {
+    console.log("It never Ends ");
+  }
+}
+
+NeverEnding();
+
+// Interfaces
+
+//  object Interface
+
+interface obj {
+  name: string;
+  age: number;
+  isAttended: boolean;
+}
+
+const personObj: obj = {
+  name: "bhanu",
+  age: 24,
+  isAttended: true,
+};
+
+console.log(personObj.age);
+
+// Interfaces for functions
+
+interface addFunction {
+  (a: number, b: number): number;
+}
+
+const twoSum: addFunction = (a: number, b: number) => {
+  return a + b;
+};
+
+console.log(twoSum(10, 20));
+
+// Custom Types
+
+type user = {
+  name: string;
+  age: number;
+  isAttended: boolean;
+};
+
+const User: user = {
+  name: "bhanu",
+  age: 24,
+  isAttended: true,
+};
+
+type mult = string | number | boolean;
+
+let Multmessage: mult = 6546545;
